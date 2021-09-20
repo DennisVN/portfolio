5f3e3c1e41329244ee52a9165e01efec0302dfd6
DennisVN <78357110+DennisVN@users.noreply.github.com>
2021-09-20 16:40:01 +0200

    Setup dummy data for Caroussel

--- src/components/Caroussel.tsx
+++ src/components/Caroussel.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Carousel } from 'react-bootstrap';
+import testimage from '../images/testimage.jpg';
 
 export default function Caroussel() {
     return (
@@ -8,33 +9,33 @@ export default function Caroussel() {
             <Carousel.Item>
                 <img
                 className="d-block w-100"
-                src="holder.js/800x400?text=First slide&bg=f5f5f5"
-                alt="First slide"
+                src={testimage}
+                alt="React Pokédex"
                 />
                 <Carousel.Caption>
-                    <h5>First slide label</h5>
+                    <h5>React Pokédex</h5>
                     <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
                 </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
                 <img
                 className="d-block w-100"
-                src="holder.js/800x400?text=Second slide&bg=eee"
-                alt="Second slide"
+                src={testimage}
+                alt="Laravel Todo App"
                 />
                 <Carousel.Caption>
-                <h5>Second slide label</h5>
+                <h5>Laravel Todo App</h5>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
                 </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
                 <img
                 className="d-block w-100"
-                src="holder.js/800x400?text=Third slide&bg=e5e5e5"
-                alt="Third slide"
+                src={testimage}
+                alt="Flappy Bert"
                 />
                 <Carousel.Caption>
-                <h5>Third slide label</h5>
+                <h5>Flappy Bert</h5>
                 <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
                 </Carousel.Caption>
             </Carousel.Item>
