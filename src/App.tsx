3aa11c26c8675673dd771ad7b6e8f72776b78626
DennisVN <78357110+DennisVN@users.noreply.github.com>
2021-09-20 15:17:59 +0200

    Replace React logo with Personalised Favicon, Add name in tab

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,14 @@ import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 import './App.css';
 import AboutMe from './components/AboutMe';
 import ContactInfo from './components/ContactInfo';
+import Footer from './components/Footer';
 import MyProjects from './components/MyProjects';
 
 function App() {
   return (
     <>
       <Router>
-          <Link to="/"><button className="btn btn-outline-light col-12 p-2" type="button"> Homepage </button></Link>
+          <Link to="/"><button className="btn btn-outline-light col-12 p-2" type="button"> homepage (minigame ?)</button></Link>
           <Link to="/aboutMe"><button className="btn btn-outline-light col-12 p-2" type="button"> about me</button></Link>
           <Link to="/contactInfo"><button className="btn btn-outline-light col-12 p-2" type="button"> contact info</button></Link>
           <Link to="/myProjects"><button className="btn btn-outline-light col-12 p-2" type="button"> my projects</button></Link>
@@ -19,7 +20,7 @@ function App() {
           <Route path="/myProjects" component={MyProjects}/>
         </Switch>
       </Router>
-      <p>testing out home page</p>
+      <Footer />
     </>
   );
 }
--- src/components/ContactInfo.tsx
+++ src/components/ContactInfo.tsx
@@ -7,6 +7,8 @@ export default function ContactInfo() {
             <p>e-mail: d.vannieuwenhuyse@gmail.com</p>
             <p>gsm : +32 472 48 69 15</p>
             <p>adress : jakkamakka dummydata</p>
+            <p>Linkedin</p>
+            <p>GitHub</p>
         </div>
     )
 }
