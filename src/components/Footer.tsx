c60ad7e09961efa0e68b898e22ce56d54ea24189
DennisVN <78357110+DennisVN@users.noreply.github.com>
2021-09-20 15:34:02 +0200

    Add Linkedin icon in Footer WIP do same for GitHub and complete Footer first

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+import { SocialIcon } from 'react-social-icons';
 
 export default function Footer() {
     return (
         <>
              <div>
                 <div className="fixed-bottom text-center" >
-                    <p>Linkedin favicon</p>
+                <SocialIcon url="https://linkedin.com/in/dennisvannieuwenhuyse" />
                     <p>GitHub favicon</p>
                 </div>
             </div>
