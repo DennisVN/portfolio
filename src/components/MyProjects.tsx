fd009a71513b45ba9b0d727f9704b4128a35f708
DennisVN <78357110+DennisVN@users.noreply.github.com>
2021-09-21 09:51:35 +0200

    Remove Carousel, replace with cards

--- src/components/MyProjects.tsx
+++ src/components/MyProjects.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import Caroussel from './Caroussel'
 
 export default function MyProjects() {
     return (
         <>
-            <Caroussel />
+            {/* <Caroussel /> */}
+            <p>Projects</p>
         </>
     )
 }
