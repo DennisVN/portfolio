369c7da04bb76a9e66a91d475ee1626ddbdb3202
DennisVN <78357110+DennisVN@users.noreply.github.com>
2021-09-22 21:50:37 +0200

    Make name and catchphrase reponsive, add media query and bootstrap col

--- src/components/Name.tsx
+++ src/components/Name.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
 // import './App.css';
 
 export default function Name() {
     return (
         <>
-            <div className="fancyName" >DENNIS</div><hr />
-            <div className="fancySubName">Let me fix this for you</div>
+        <Container>
+            <Row>
+                <Col xs={6} md={12}><div className="fancyName" >DENNIS</div></Col><hr />
+                <Col xs={6} md={12}><div className="fancySubName">Let me fix this for you</div></Col>
+            </Row>  
+        </Container>
         </>
     )
 }
