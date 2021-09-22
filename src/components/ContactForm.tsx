fbf63e2964385b32df742b0f106a488146aca500
DennisVN <78357110+DennisVN@users.noreply.github.com>
2021-09-22 11:22:02 +0200

    Update contact form

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -9,12 +9,12 @@ export default function ContactForm() {
         <div className="formContainer">
             <Form>
                 <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-                    <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="name@example.com" />
+                    <Form.Label>Your e-mail:</Form.Label>
+                    <Form.Control type="email" placeholder="smartperson@hireme.com" />
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
                     <Form.Label>Example textarea</Form.Label>
-                    <Form.Control as="textarea" rows={3} />
+                    <Form.Control as="textarea" rows={3} placeholder="Hi Dennis, let's work !" />
                 </Form.Group>
             </Form>
         </div>
