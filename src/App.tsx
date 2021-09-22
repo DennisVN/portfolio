113685e26c8b105cd19d8f6d585931edd393f283
DennisVN <78357110+DennisVN@users.noreply.github.com>
2021-09-22 21:33:27 +0200

    Make Heading links bootsrap responsive

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import Footer from './components/Footer';
 import MyProjects from './components/MyProjects';
 import Name from './components/Name';
 import ClimbingBoxLoader from "react-spinners/ClimbingBoxLoader";
+import { Col, Container, Row } from 'react-bootstrap';
 
 
 function App() {
@@ -28,20 +29,24 @@ function App() {
         <ClimbingBoxLoader color={"#66ff00"} loading={loading} size={30}/>
         : 
         <>
-        <Header />
-        <Router>
-              <Link to="/"><button className="btn btn-outline-black col-3" type="button"> HOME</button></Link>
-              <Link to="/myProjects"><button className="btn btn-outline-black col-3" type="button">PROJECTS</button></Link>
-              <Link to="/aboutMe"><button className="btn btn-outline-black col-3 " type="button"> ABOUT</button></Link>
-              <Link to="/contactInfo"><button className="btn btn-outline-black col-3 " type="button"> CONTACT</button></Link>
-          <Switch>
-            <Route path="/" exact component={Name} />
-            <Route path="/aboutMe" component={AboutMe} />
-            <Route path="/contactInfo" component={ContactInfo}/>
-            <Route path="/myProjects" component={MyProjects}/>
-          </Switch>
-        </Router>
-        <Footer />
+        <Container>
+          <Row>
+            <Header />
+              <Router>
+                    <Col xs={12} md={3}><Link to="/"><button className="btn btn-outline-black col-3" type="button"> HOME</button></Link></Col>
+                    <Col xs={12} md={3}><Link to="/myProjects"><button className="btn btn-outline-black col-3" type="button">PROJECTS</button></Link></Col>
+                    <Col xs={12} md={3}><Link to="/aboutMe"><button className="btn btn-outline-black col-3 " type="button"> ABOUT</button></Link></Col>
+                    <Col xs={12} md={3}><Link to="/contactInfo"><button className="btn btn-outline-black col-3 " type="button"> CONTACT</button></Link></Col>
+                <Switch>
+                  <Route path="/" exact component={Name} />
+                  <Route path="/aboutMe" component={AboutMe} />
+                  <Route path="/contactInfo" component={ContactInfo}/>
+                  <Route path="/myProjects" component={MyProjects}/>
+                </Switch>
+              </Router>
+              <Footer />
+          </Row>
+        </Container>
         </>
       }
 
