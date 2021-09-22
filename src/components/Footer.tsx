5ecc9ed206525691ec0a0cf27c5d6f597516f2a0
DennisVN <78357110+DennisVN@users.noreply.github.com>
2021-09-22 22:22:12 +0200

    Make footer responsive, fix to correct fixed position, WIP make mobile menu first

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
 
 export default function Footer() {
     return (
         <>
              <div>
-                <div className="fixed-bottom text-center" >
-                    <p>"If you believe you are here to create, seek love & knowledge and you will find a way" - Modern Life Is War</p>
-                </div>
+                 <Container>
+                     <Row>
+                         <Col>
+                            <div className="fixed-bottom" >
+                                <p>"If you believe you are here to create, seek love & knowledge and you will find a way" - Modern Life Is War</p>
+                            </div>
+                        </Col>
+                    </Row>
+                </Container>
             </div>
         </>
     
