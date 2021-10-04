73f24d4e84b40c02184d2e2106829a848d53a5b8
DennisVN <78357110+DennisVN@users.noreply.github.com>
2021-10-04 12:22:14 +0200

    Redo contact form

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -7,7 +7,7 @@ export default function ContactForm() {
         <>
         <div className="formContainer">
             <Form>
-            <Form.Group className="p-3" controlId="exampleForm.ControlTextarea1">
+                <Form.Group className="p-3" controlId="exampleForm.ControlTextarea1">
                     <Form.Label>Let's work </Form.Label>
                     <Form.Control as="textarea" rows={10} placeholder="Thank you for checking out my page. By now you have a better idea about me and my purpose, so don't hesitate to contact me ! I love to collaborate, click on the social links to get started ." disabled/>
                 </Form.Group>
