fc408393672076141d9512d799093a35483bb9fc
DennisVN <78357110+DennisVN@users.noreply.github.com>
2021-10-04 11:19:29 +0200

    Remove social links from header for better UI

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,8 +5,8 @@ export default function Header() {
     return (
         <>
         <div style={{display: 'flex', justifyContent: 'flex-end', margin:"3px"}} className="header p-1" >
-        <SocialIcon url="https://linkedin.com/in/dennisvannieuwenhuyse" target="_blank" />
-        <SocialIcon url="https://github.com/DennisVN" target="_blank" />
+        {/* <SocialIcon url="https://linkedin.com/in/dennisvannieuwenhuyse" target="_blank" />
+        <SocialIcon url="https://github.com/DennisVN" target="_blank" /> */}
         </div>
         <div style={{display: 'flex', justifyContent: 'flex-start'}} className="header m-2" >
         <p>DVN PORTFOLIO (Full Stack Dev)</p><hr />
