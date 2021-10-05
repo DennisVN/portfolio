925cbd95fee647d11b7d8fc91212b2ffe97f8a0b
DennisVN <78357110+DennisVN@users.noreply.github.com>
2021-10-05 10:06:26 +0200

    Add icons on project card, remove weather app card

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Card } from 'react-bootstrap';
 import { Button } from 'react-bootstrap';
+import { FaReact, FaJsSquare } from "react-icons/fa";
+import { SiVisualstudiocode } from "react-icons/si";
 // import { CardGroup } from 'react-bootstrap';
 
 
@@ -14,14 +16,15 @@ export default function ProjectCard() {
                         <Card.Text>
                             Pokédex app in React, not your average tutorial dex.
                             First page created by the amazing Jonathan, Pokédex page by me .
-                            Used PokéAPI for this project.  
+                            Used PokéAPI for this project.<br/>
+                            <FaReact />  <FaJsSquare /> <SiVisualstudiocode />
                         </Card.Text>
                         <Button variant="dark" href="https://pokedex-dennis-jonathan.netlify.app/" target="_blank">Deployed Here</Button><br/>
                         <Button variant="dark" href="https://github.com/DennisVN/pokedex" target="_blank">GitHub Repo</Button>
                     </Card.Body>
                 </Card>
                 <div className="cardContainer text-center">
-                <Card style={{ border:'primary' }}>
+                {/* <Card style={{ border:'primary' }}>
                     <Card.Body>
                         <Card.Title>WEATHER APP<hr/></Card.Title>
                         <Card.Text>
@@ -32,7 +35,7 @@ export default function ProjectCard() {
                         <Button variant="dark" href="https://dennisvn.github.io/weather-app/" target="_blank">Deployed Here</Button><br/>
                         <Button variant="dark" href="https://github.com/DennisVN/weather-app" target="_blank">GitHub Repo</Button>
                     </Card.Body>
-                </Card>
+                </Card> */}
             </div>
             </div>
             
