2846abdd2ba7eaf7e338b0e651c268c5c9e3ca2e
DennisVN <78357110+DennisVN@users.noreply.github.com>
2021-10-25 13:16:19 +0200

    Add padding between cards for better UI

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import AboutCard from './AboutCard';
+import { Container, Row, Col } from 'react-bootstrap';
 
 export default function AboutMe() {
     return (
-        <AboutCard />
+        <Container>
+            <Row>
+                <Col xs={12} md={12} ><AboutCard /></Col>
+            </Row>
+        </Container>
     )
 }
