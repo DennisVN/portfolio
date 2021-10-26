0b6a63e3bfb05b3115b04323d9d1f7e46eb9ce2d
DennisVN <78357110+DennisVN@users.noreply.github.com>
2021-10-26 19:33:50 +0200

    Align icons on project card

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -2,11 +2,12 @@ import React from 'react';
 import AboutCard from './AboutCard';
 import { Container, Row, Col } from 'react-bootstrap';
 import { motion } from 'framer-motion';
+import { animationOne, transition } from '../animations';
 
 export default function AboutMe() {
     return (
         <>
-        <motion.div initial={{scaleY: 0}} animate={{scaleY: 1}} exit={{scaleY: 0}} transition={{duration:1}} >
+        <motion.div initial='out' animate='in' exit='out' variants={animationOne} transition={transition}>
             <Container>
                 <Row>
                     <Col xs={12} md={12} ><AboutCard /></Col>
--- src/components/Name.tsx
+++ src/components/Name.tsx
@@ -3,6 +3,7 @@ import { Col, Container, Row } from 'react-bootstrap';
 // import './App.css';
 import {motion} from 'framer-motion';
 import { animationOne, transition } from '../animations';
+import { Link } from 'react-router-dom';
 
 export default function Name() {
     return (
@@ -12,6 +13,15 @@ export default function Name() {
                 <Row>
                     <Col xs={6} md={12}><div className="fancyName" >DENNIS</div></Col><hr />
                     <Col xs={6} md={12}><div className="fancySubName">Let me fix this for you</div></Col>
+                </Row>
+                <Row>
+                <Col xs={12} md={12}>
+                    <div className="redirection font-mono font-family: ui-monospace font-family:monospace">
+                        {/* <Link to="/aboutMe" style={{textDecoration: 'none', color: '#bb25fb'}}>Learn more about me</Link><br/>
+                        <Link to="/myProjects" style={{textDecoration: 'none', color:'#bb25fb'}}>Check out what i have been doing</Link><br/>
+                        <Link to="/contactInfo" style={{textDecoration: 'none', color: '#bb25fb'}}>Let's get in touch ! </Link><br/> */}
+                    </div>
+                </Col>
                 </Row>  
             </Container>
         </motion.div>
