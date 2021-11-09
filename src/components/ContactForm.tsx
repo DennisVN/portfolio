c03758bc54a9e3fd1fdfae9dcf2c73ca3ae215e9
DennisVN <78357110+DennisVN@users.noreply.github.com>
2021-11-09 14:29:33 +0100

    Linkt fix for Netlify

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, Col, Container, Row } from 'react-bootstrap';
+import { Card, Container, Row } from 'react-bootstrap';
 import ContactCard from './ContactCard';
 
 
@@ -8,20 +8,18 @@ export default function ContactForm() {
         <>
         <Container>
             <Row>
-                {/* <Col className="contactSpecs px-10" xs={12} md={12}> */}
-                    <div className="formContainer">
-                        <Card style={{ border:'primary'}}>
-                            <Card.Body>
-                                <Card.Title>CONTACT ME<hr/></Card.Title>
-                                <Card.Text>
-                                    Let's build the future together ! <br/>
-                                    If you like what you see, click one of the icons below to get in touch.<br></br>
-                                </Card.Text>
-                                <ContactCard />
-                            </Card.Body>
-                        </Card>
-                    </div>
-                {/* </Col> */}
+                <div className="formContainer">
+                    <Card style={{ border:'primary'}}>
+                        <Card.Body>
+                            <Card.Title>CONTACT ME<hr/></Card.Title>
+                            <Card.Text>
+                                Let's build the future together ! <br/>
+                                If you like what you see, click one of the icons below to get in touch.<br></br>
+                            </Card.Text>
+                            <ContactCard />
+                        </Card.Body>
+                    </Card>
+                </div>
             </Row>
         </Container>
 
