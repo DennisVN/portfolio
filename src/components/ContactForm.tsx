cd7a87b5a7b3cb8f75eec41af27977f116aec40c
DennisVN <78357110+DennisVN@users.noreply.github.com>
2021-11-09 11:33:29 +0100

    Solve card centering issue for mobile

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -1,25 +1,30 @@
 import React from 'react';
-import { Card } from 'react-bootstrap';
+import { Card, Col, Container, Row } from 'react-bootstrap';
 import ContactCard from './ContactCard';
 
 
 export default function ContactForm() {
     return (
         <>
-            <div className="formContainer text-center">
-                <Card style={{ border:'primary'}}>
-                    <Card.Body>
-                        <Card.Title>CONTACT ME<hr/></Card.Title>
-                        <Card.Text>
-                            Let's build the future together ! <br/>
-                            If you like what you see, click one of the icons below to get in touch.<br></br>
-                            <ContactCard />
-                            <br/>
-                            <p className="dadJoke">Thanks for checking out my Portfolio. See you soon ! </p>
-                        </Card.Text>
-                    </Card.Body>
-                </Card>
-            </div>
+        <Container>
+            <Row>
+                <Col className="contactSpecs px-10" xs={12} md={12}>
+                    <div className="formContainer">
+                        <Card style={{ border:'primary'}}>
+                            <Card.Body>
+                                <Card.Title>CONTACT ME<hr/></Card.Title>
+                                <Card.Text>
+                                    Let's build the future together ! <br/>
+                                    If you like what you see, click one of the icons below to get in touch.<br></br>
+                                </Card.Text>
+                                <ContactCard />
+                            </Card.Body>
+                        </Card>
+                    </div>
+                </Col>
+            </Row>
+        </Container>
+
         </>
     )
 }
