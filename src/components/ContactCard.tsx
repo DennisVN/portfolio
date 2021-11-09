bcffe0e5fc0cc35d2a423464ec9ce9d41aa595da
DennisVN <78357110+DennisVN@users.noreply.github.com>
2021-11-09 11:48:17 +0100

    Look for sweetspot of card size on mobile

--- src/components/ContactCard.tsx
+++ src/components/ContactCard.tsx
@@ -3,19 +3,15 @@ import { SocialIcon } from 'react-social-icons';
 
 export default function ContactCard() {
     return (
-        <div>
-
-                    
-                        <SocialIcon className="m-2" url="mailto:d.vannieuwenhuyse@gmail.com" bgColor="black" /> <p className="linkedText">d.vannieuwenhuyse@gmail.com </p>
-                        <SocialIcon className="m-2" url="https://linkedin.com/in/dennisvannieuwenhuyse" target="_blank" bgColor="black" /> <p className="linkedText">/dennisvannieuwenhuyse</p>
-                        <SocialIcon className="m-2" url="https://github.com/DennisVN" target="_blank" bgColor="black" /><p className="linkedText"> github.com/DennisVN</p>
-                        <SocialIcon className="m-2" url="https://discordapp.com/users/4590/" target="_blank" bgColor="black" /> <p className="linkedText"> discordapp.com/users/4590/ </p>
-                        <br/>
-                        <img src="/images/grayscale.png" className="img-fluid mx-auto" alt="rare display of a wild Beaucoupdargent"></img>
-                        <br/>
-                        <p className="dadJoke">Thanks for checking out my Portfolio.<br/> See you soon ! </p>
-
-
+        <div>     
+            <SocialIcon className="m-2" url="mailto:d.vannieuwenhuyse@gmail.com" bgColor="black" /> <p className="linkedText">d.vannieuwenhuyse@gmail.com </p>
+            <SocialIcon className="m-2" url="https://linkedin.com/in/dennisvannieuwenhuyse" target="_blank" bgColor="black" /> <p className="linkedText">/dennisvannieuwenhuyse</p>
+            <SocialIcon className="m-2" url="https://github.com/DennisVN" target="_blank" bgColor="black" /><p className="linkedText"> github.com/DennisVN</p>
+            <SocialIcon className="m-2" url="https://discordapp.com/users/4590/" target="_blank" bgColor="black" /> <p className="linkedText"> discordapp.com/users/4590/ </p>
+            <br/>
+            <img src="/images/grayscale.png" className="img-fluid mx-auto" alt="rare display of a wild Beaucoupdargent"></img>
+            <br/>
+            <p className="dadJoke">Thanks for checking out my Portfolio.<br/> See you soon ! </p>
         </div>
     )
 }
